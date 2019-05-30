4d41caed793b2bbae94e753c74cd856f7e25006c
Tianli Yu <tianliyu@usc.edu>
2019-05-30 12:24:32 -0700

    figured out DNAC API

--- WebTerm/urls.py
+++ WebTerm/urls.py
@@ -4,5 +4,6 @@ from WebTerm import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
-    url(r'^terminal/$', views.terminal, name='terminal')
+    url(r'^terminal/$', views.terminal, name='terminal'),
+    url(r'^devices/$', views.devices, name='devices')
 ]
--- WebTerm/views.py
+++ WebTerm/views.py
@@ -3,14 +3,17 @@ import socket
 from django.views.generic import TemplateView
 
 from django.http import HttpResponse
-from threading import Timer
+import requests
+session = requests.Session()
+
+login_url = "https://172.27.250.16/api/system/v1/auth/login"
+check_device_url = "https://172.27.250.16/api/rdm/v1/device"
+username = 'admin'
+password = 'Maglev123'
 
 clientPassword = "admin"
 clientUsername = "admin"
 clientIP = "40.0.0.10"
-# hostPublic = "pnp-scd-dev03.cisco.com"
-# hostLocal = "172.23.165.87"
-# portToHostTerminal = "8080"
 timeout_sec = "20"
 
 
@@ -21,6 +24,41 @@ def get_open_port():
     port = s.getsockname()[1]
     s.close()
     return port
+
+
+def parse_dict_cookies(value):
+    result = {}
+    for item in value.split(';'):
+        item = item.strip()
+        if not item:
+            continue
+        if '=' not in item:
+            result[item] = None
+            continue
+        name, value = item.split('=', 1)
+        result[name] = value
+    return result
+
+
+def devices(request):
+
+    response = session.get(login_url, auth=(username, password), verify=False)
+
+    C = parse_dict_cookies(response.headers['Set-Cookie'])
+    # for key, value in C:
+    #     session.cookies.set(key, value)
+
+    r = session.get(check_device_url, cookies=C)
+
+    # if r.status_code != 200:
+    #     response = session.get(login_url, auth=(username, password), verify=False)
+    #
+    #     C = parse_dict_cookies(response.headers['Set-Cookie'])
+
+    # print(r.status_code)
+    #
+    # print(r.text)
+    return HttpResponse(r.text)
 
 
 def terminal(request):
